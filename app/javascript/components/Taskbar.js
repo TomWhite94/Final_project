2d83024bdb8f271db37816d52d4b4ca78f8a039a
github username <github email address>
2020-06-22 17:20:25 +0100

    Search bar dropdown

--- app/javascript/components/Taskbar.js
+++ app/javascript/components/Taskbar.js
@@ -2,44 +2,40 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
+import { AsyncTypeahead } from 'react-bootstrap-typeahead'
 
-let ARTIST_SEARCH = "https://api.songkick.com/api/3.0/search/artists.json?apikey=XyKG4KDNOliuaDev&query="
+
+let ARTIST_SEARCH = "https://api.songkick.com/api/3.0/search/artists.json?apikey=XyKG4KDNOliuaDev&per_page=5&page=1&query="
 
 class Taskbar extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            artist_search_result: [],
-            artist_search: "",
-            numOfPages: 0,
-            page: 0
+            artistSearchResult: [],
+            isSearching: false 
         }
-        this.handleChange = this.handleChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
         
     }
 
-   
-
-    handleChange(event){
-       
-        
-        
-        fetch(ARTIST_SEARCH + event.target.value)
+    handleSearch(query) { 
+        this.setState({isSearching: true})
+        fetch(ARTIST_SEARCH + query)
             .then(response => response.json())
             .then(data => {
                 if (data.resultsPage.status == "ok" && data.resultsPage.results.hasOwnProperty("artist")) {
                     this.setState({
-                        artist_search_result: data.resultsPage.results.artist, 
-                        page: data.resultsPage.page, 
-                        numOfPages: Math.ceil(parseInt(data.resultsPage.totalEntries) / parseInt(data.resultsPage.perPage)),
-                        // artist_search: event.target.value
+                        artistSearchResult: data.resultsPage.results.artist, 
+                        // page: data.resultsPage.page, 
+                        // numOfPages: Math.ceil(parseInt(data.resultsPage.totalEntries) / parseInt(data.resultsPage.perPage)),
+                        isSearching: false,
+                        
                     })
                 } else {
                     this.setState({
-                        artist_search_result: []
+                        artistSearchResult: [],
+                        isSearching: false
                     })
                 }
 
@@ -48,37 +44,37 @@ class Taskbar extends Component {
             
 
 render() {
-    console.log(this.state.artist_search_result)
+    console.log(this.state.artistSearchResult)
     return(
         
         <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" name="artist_search" placeholder="Artist Search" className="mr-sm-2" onChange={this.handleChange}/>
-
-      <ul>
-        {this.state.artist_search_result.map((resp, id) => (
-            
-            <li key={id}>{resp.displayName}</li>
-            
-        ))}
-            </ul>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+                </Nav>
+    
+                <AsyncTypeahead
+                        id="basic-typeahead-example"
+                        isLoading={this.state.isSearching}
+                        labelKey="displayName"
+                        onSearch={this.handleSearch}
+                        options={this.state.artistSearchResult}
+                        placeholder="Artist Search"
+                        // selected={selected}
+                    />
+                
+            </Navbar.Collapse>
+        </Navbar>
    
     
             
